64321b369f81407ab37550e8a9c0a5708adade67
Alisha Hunnewell <alisha.hunnewell@gmail.com>
2026-09-22 16:18:24 -0400

    Add checkstd.cpp and variable practice

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 int main()
 {
-    std::cout << "Hello, World!";
-    return 0;
+    std::cout << "Hello, World!";   //std::cout lives in the isostream lib
+    return 0;                       //comments at different lengths are hard to read
 }
---
+++ variablepractice.cpp
@@ -0,0 +1,11 @@
+#include <iostream>    // for std::cout
+
+int main()
+{
+    std::cout << 5;    //prints literal number '5'
+    std::cout << -6.7; //prints literal number '-6.7'
+    std::cout << 'H';   //prints literal character 'H'
+    std::cout << "Hello"; //prints literal string "Hello"
+
+    return 0;
+}
