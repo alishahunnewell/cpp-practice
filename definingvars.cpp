2c991cce6aa40187c815105c0eb0ab5a62d66934
Alisha Hunnewell <alisha.hunnewell@gmail.com>
2026-09-24 12:40:29 -0400

    Add variable assignment

---
+++ definingvars.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+    int x; //defines a variable named x (of type int)
+    int y, z; //defines two integer varaibles
+
+    int width; //define a variable named width
+    width = 5; //assign the value 5 to the variable width
+
+    std::cout << width; //prints 5
+    width = 7; //change value stored in varaiable width to 7
+
+    std::cout << width; //prints 7
+    return 0;
+}
